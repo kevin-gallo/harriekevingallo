efc01af0aea42a0c9f1672149603c5709020b6e9
kevin-gallo <harriekevingallo@gmail.com>
2026-07-27 19:56:43 +0800

    fix: redirect fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "harriekevingallo.vercel.app",
+          },
+        ],
+        destination: "https://harriekevingallo.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
